7aa8289f86ab555f92aa2be8a1733ef0e6b23eb9
KaterinaLyashenko <ekl18062003@yandex.ru>
2022-11-20 12:22:29 +0300

    config changes

--- dao/model/movie.py
+++ dao/model/movie.py
@@ -11,7 +11,7 @@ class Movie(db.Model):
     description = db.Column(db.String(300))
     trailer = db.Column(db.String(100))
     year = db.Column(db.Integer)
-    raiting = db.Column(db.Float)
+    rating = db.Column(db.Float)
     genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
     genre = db.relationship("Genre")
     director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
@@ -23,6 +23,6 @@ class MovieSchema(Schema):
     description = fields.Str()
     trailer = fields.Str()
     year = fields.Int()
-    raiting = fields.Float()
+    rating = fields.Float()
 
 
